1fc0cbc1c872c88e531a84d9a09ed47c59da5264
gastondeganis <gastondeganis@gmail.com>
2023-05-19 20:26:29 -0300

    cambioos

--- TP2/Ej2/main.cpp
+++ TP2/Ej2/main.cpp
@@ -10,7 +10,18 @@ long long N, M;
 //se piensa como listas de adyacencias por cada vertice, espacio: O(N + M)
 vector<vector<int>> g;
 vector<vector<int>> g_t; //traspuesta
-vector<bool> tieneIn0;
+vector<bool> tiene_in;
+vector<bool> fue_tirada;
+
+void tirar_fichas(int v){
+    fue_tirada[v] = true;
+    for (int u : g[v]){
+        if (!fue_tirada[u]){
+            tirar_fichas(u);
+        }
+    }
+}
+
 
 /*idea:
 1- buscar vertices con grado de entrada 1 y tirarlos con DFS
@@ -21,14 +32,29 @@ vector<bool> tieneIn0;
 int main() {
     cin >> N >> M;
 
+    tiene_in.assign(N, false);
+    fue_tirada.assign(N, false);
+    cant_en_pie = N;
+
     g.resize(N + 1, vector<int>(0));
 
     while (M--) {
         int u, v;
         cin >> u >> v;
+        tiene_in[v]=true;
         g[u].push_back(v);
-        g_t[v].push_back(u);
     }
+
+    // Tiro todas las que tienen grado de entrada 0
+    for (int i =0; i<N; i++){
+        if(!tiene_in[i]){
+            tirar_fichas(i);
+        }
+    }
+
+    // A partir de acá sólo queda al menos una comp conexa con ciclo.
+
+    
 
     return 0;
 }
