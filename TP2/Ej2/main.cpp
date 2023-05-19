31018d102ec987836c6dcf69c12c099ea44b28c5
gastondeganis <gastondeganis@gmail.com>
2023-05-19 20:54:57 -0300

    ya casi

--- TP2/Ej2/main.cpp
+++ TP2/Ej2/main.cpp
@@ -13,6 +13,13 @@ vector<vector<int>> g_t; //traspuesta
 vector<bool> tiene_in;
 vector<bool> fue_tirada;
 
+// Estos son sólo para hacer Kosaraju
+vector<int> componente, orden;
+vector<bool> usados;
+
+// Vector con los resultados
+vector<int> res;
+
 void tirar_fichas(int v){
     fue_tirada[v] = true;
     for (int u : g[v]){
@@ -21,6 +28,53 @@ void tirar_fichas(int v){
         }
     }
 }
+
+
+void dfs1(int v){
+    usados[v] = true;
+    for(int u : g[v]){
+        if(usados[u]){
+            dfs1(u);
+        }
+    }
+    orden.push_back(v);
+}
+
+void dfs2(int v){
+    usados[v]=true;
+    componente.push_back(v);
+    for (int u : g_t[v]){
+        if (!usados[u]){
+            dfs2(u);
+        }
+    }
+}
+
+
+void kosaraju(){
+    usados.resize(N+1, false);
+    for (int i =1; i<N+1; i++){
+        if (!fue_tirada[i] && !usados[i]){
+            dfs1(i);
+        }
+    }
+    reverse(orden.begin(), orden.end());
+    usados.resize(N+1, false);
+
+    for (int v :orden){
+        if(!usados[v]){
+            dfs2(v);
+        
+        // Acá tengo la componente
+
+        res.push_back(min(componente));
+
+        componente.clear();
+        
+        }
+    }
+}
+
 
 
 /*idea:
@@ -32,29 +86,36 @@ void tirar_fichas(int v){
 int main() {
     cin >> N >> M;
 
-    tiene_in.assign(N, false);
-    fue_tirada.assign(N, false);
+    tiene_in.assign(N+1, false);
+    fue_tirada.assign(N+1, false);
     cant_en_pie = N;
 
     g.resize(N + 1, vector<int>(0));
+    g_t.resize(N + 1, vector<int>(0));
 
     while (M--) {
         int u, v;
-        cin >> u >> v;
+        cin >> u >> v;  
         tiene_in[v]=true;
         g[u].push_back(v);
+        g_t[v].push_back(u);
     }
 
     // Tiro todas las que tienen grado de entrada 0
-    for (int i =0; i<N; i++){
+    for (int i =1; i<N+1; i++){
         if(!tiene_in[i]){
+            res.push_back(i);
             tirar_fichas(i);
         }
     }
 
+    // Tenemos: Nodos tirados -> no nos importa hacer nada mas con ellos. 
+
     // A partir de acá sólo queda al menos una comp conexa con ciclo.
 
-    
+    kosaraju();
+
+    res.sort();
 
     return 0;
 }
