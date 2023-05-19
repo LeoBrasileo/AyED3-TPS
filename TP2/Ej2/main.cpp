d9adcf88986fdbdd843f834af1804f312cd8680e
LeoBrasileo <leobraginski@gmail.com>
2023-05-19 20:18:30 -0300

    idea cfc

--- TP2/Ej2/main.cpp
+++ TP2/Ej2/main.cpp
@@ -9,6 +9,14 @@ long long N, M;
 //g es la representacion del grafo
 //se piensa como listas de adyacencias por cada vertice, espacio: O(N + M)
 vector<vector<int>> g;
+vector<vector<int>> g_t; //traspuesta
+vector<bool> tieneIn0;
+
+/*idea:
+1- buscar vertices con grado de entrada 1 y tirarlos con DFS
+2- buscar componentes fuertemente conexas (Kosaraju)
+3- por cada ciclo encontrado, es decir cada comp fuertemente conexa, tirar las fichas con menor num de vertice
+ */
 
 int main() {
     cin >> N >> M;
@@ -19,6 +27,7 @@ int main() {
         int u, v;
         cin >> u >> v;
         g[u].push_back(v);
+        g_t[v].push_back(u);
     }
 
     return 0;
