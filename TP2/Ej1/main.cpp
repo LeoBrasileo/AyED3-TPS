05159eb8972a6a1d45d793a9b488294d5cd3905e
LeoBrasileo <leobraginski@gmail.com>
2023-05-19 13:50:45 -0300

    judge pls

--- TP2/Ej1/main.cpp
+++ TP2/Ej1/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 typedef pair<int, int> arista;
 
-long long N, M;
+long long N, M, jugadasTotales;
 //g es la representacion del grafo
 //se piensa como listas de adyacencias por cada vertice, espacio: O(N + M)
 vector<arista> puentes;
@@ -17,7 +17,7 @@ vector<bool> visitados;
 vector<int> tin, low;
 int timer;
 
-void dfsPuentes(int v, int padre = -1) {
+void dfsPuentes(int v, int padre) {
     visitados[v] = true;
     tin[v] = low[v] = timer++;
     for (int to : g[v]) {
@@ -30,6 +30,7 @@ void dfsPuentes(int v, int padre = -1) {
             if (low[to] > tin[v])
             {
                 puentes.push_back({v, to});
+                puentes.push_back({to, v});
             }
         }
     }
@@ -37,12 +38,12 @@ void dfsPuentes(int v, int padre = -1) {
 
 void buscarPuentes() {
     timer = 0;
-    visitados.assign(N+1, false);
-    tin.assign(N+1, -1);
-    low.assign(N+1, -1);
-    for (int i = 0; i < N; ++i) {
+    visitados = vector<bool>(N+1, false);
+    tin = vector<int>(N+1, -1);
+    low = vector<int>(N+1, -1);
+    for (int i = 1; i <= N; i++) {
         if (!visitados[i])
-            dfsPuentes(i);
+            dfsPuentes(i, 0);
     }
 }
 
@@ -57,9 +58,9 @@ void dfsComponentes(int v, int &tamComponente){
 }
 
 void calcVerticesxComponente(vector<int> &verticesPorComponente){
-    visitados.assign(N+1, false);
+    visitados = vector<bool>(N+1, false);
 
-    for(int i = 1; i < N; i++){
+    for(int i = 1; i <= N; i++){
         if(!visitados[i]){
             int tamComponente = 0;
             dfsComponentes(i, tamComponente);
@@ -79,7 +80,6 @@ double probaGanar() {
         int u = puente.first;
         int v = puente.second;
         g[u].erase(find(g[u].begin(), g[u].end(), v));
-        g[v].erase(find(g[v].begin(), g[v].end(), u));
     }
 
     //calcular componentes conexas y cantidad de vertices en cada una de ellas
@@ -90,27 +90,28 @@ double probaGanar() {
     //donde Vi es la cantidad de vertices en la componente i
     long long jugadasGanadas = 0;
     for(int vi : verticesPorComponente){
-        jugadasGanadas += vi * (vi - 1) / 2;
+        if (vi > 1)
+            jugadasGanadas += (vi * (vi - 1)) / 2;
     }
-    long long jugadasTotales = N * (N - 1) / 2;
 
-    return (double)jugadasGanadas / (double)jugadasTotales;
+    return (double)jugadasGanadas / jugadasTotales;
 }
 
 int main() {
     cin >> N >> M;
 
+    jugadasTotales = (N * (N - 1)) / 2;
+
     g.resize(N + 1, vector<int>(0));
 
-    for (int i = 0; i < M; i++) {
+    while (M--) {
         int u, v;
         cin >> u >> v;
         g[u].push_back(v);
         g[v].push_back(u);
     }
 
-    double ganar = probaGanar();
-    double res = 1.0 - ganar;
+    double res = 1.0 - probaGanar();
 
     cout << fixed;
     cout << setprecision(5) << res;
