b0a93508744e04c849091bc30096738cfccd6ffd
LeoBrasileo <leobraginski@gmail.com>
2023-05-19 00:56:21 -0300

    Tukigana

--- TP2/Ej1/main.cpp
+++ TP2/Ej1/main.cpp
@@ -1,83 +1,111 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <algorithm>
 
 using namespace std;
 
-enum NODO_ESTADO {
-    NO_LO_VI = -1, EMPECE_A_VER = 0, TERMINE_DE_VER = 1
-};
+typedef pair<int, int> arista;
 
 int N, M;
 //g es la representacion del grafo
-//se piensa como map de vertices a vertices vecinos, espacio: O(N^2)
+//se piensa como listas de adyacencias por cada vertice, espacio: O(N + M)
+vector<arista> puentes;
 vector<vector<int>> g;
-vector<int> estado;
-vector<vector<int>> treeEdges;
-vector<int> backConExtremoInferiorEn;
-vector<int> backConExtremoSuperiorEn;
-vector<int> memo;
 
-void dfs(int v, int padre = -1) {
-    estado[v] = EMPECE_A_VER;
+vector<bool> visitados;
+vector<int> tin, low;
+int timer;
 
-    for (int u: g[v]) {
-        if (estado[u] == NO_LO_VI) {
-            treeEdges[v].push_back(u);
-            dfs(u, v);
-        } else if (u != padre) {
-            backConExtremoInferiorEn[v]++;
-            backConExtremoSuperiorEn[u]++;
+void dfsPuentes(int v, int padre = -1) {
+    visitados[v] = true;
+    tin[v] = low[v] = timer++;
+    for (int to : g[v]) {
+        if (to == padre) continue;
+        if (visitados[to]) {
+            low[v] = min(low[v], tin[to]);
+        } else {
+            dfsPuentes(to, v);
+            low[v] = min(low[v], low[to]);
+            if (low[to] > tin[v])
+            {
+                puentes.push_back({v, to});
+            }
         }
     }
-
-    estado[v] = TERMINE_DE_VER;
 }
 
-int cubren(int v, int p = -1) {
-    if (memo[v] != -1) return memo[v];
-    int res = 0;
-    res += backConExtremoInferiorEn[v];
-    res -= backConExtremoSuperiorEn[v];
-    for (int hijo: treeEdges[v])
-        if (hijo != p) {
-            res += cubren(hijo, v);
+void dfsComponentes(int v, int &tamComponente){
+    visitados[v] = true;
+    tamComponente++;
+    for (int u : g[v]) {
+        if (!visitados[u]) {
+            dfsComponentes(u, tamComponente);
         }
-    memo[v] = res;
-    return res;
+    }
 }
 
-double calcularJugadas() {
-    int totales = N * (N - 1) / 2;
+void calcVerticesxComponente(vector<int> &verticesPorComponente){
+    visitados.assign(N, false);
 
-    //codigo dfs y busqueda de puentes robado de clase 8
-    //clase recorridos (BFS y DFS)
-    int componentes = 0;
-    for (int i = 1; i <= N; i++) {
-        if (estado[i] == NO_LO_VI) {
-            dfs(i);
-            componentes++;
+    for(int i = 1; i < N; i++){
+        if(!visitados[i]){
+            int tamComponente = 0;
+            dfsComponentes(i, tamComponente);
+            verticesPorComponente.push_back(tamComponente);
         }
     }
-    int puentes = 0;
-    for (int i = 1; i <= N; i++) {
-        if (cubren(i) == 0) {
-            puentes++;
-        }
-    }
-    puentes -= componentes;
+}
 
-    return 1 / (double) totales;
+void buscarPuentes() {
+    timer = 0;
+    visitados.assign(N, false);
+    tin.assign(N, -1);
+    low.assign(N, -1);
+    for (int i = 0; i < N; ++i) {
+        if (!visitados[i])
+            dfsPuentes(i);
+    }
+}
+
+int calcularJugadasGanadas() {
+    //codigo dfsPuentes y busqueda de puentes de https://cp-algorithms.com/graph/bridge-searching.html#implementation
+    //tambien se usa conocimiento de clase recorridos (BFS y DFS)
+    buscarPuentes();
+
+    //eliminar puentes del grafo original,
+    //me quedo con un grafo en el que se que Tuki gana si elige 2 vertices de la misma componente
+    for (int i = 0; i < puentes.size(); i++){
+        int u = puentes[i].first;
+        int v = puentes[i].second;
+        g[u].erase(find(g[u].begin(), g[u].end(), v));
+        g[v].erase(find(g[v].begin(), g[v].end(), u));
+    }
+
+    //calcular componentes conexas y cantidad de vertices en cada una de ellas
+    vector<int> verticesPorComponente;
+    calcVerticesxComponente(verticesPorComponente);
+
+    //ganadas = sumatoria de (Vi * (Vi - 1) / 2) de i = 1 a #componentes
+    //donde Vi es la cantidad de vertices en la componente i
+    int ganadas = 0;
+    for(int i = 0; i < verticesPorComponente.size(); i++){
+        int vi = verticesPorComponente[i];
+        ganadas += vi * (vi - 1) / 2;
+    }
+
+    return ganadas;
+}
+
+double probaPerder(int jugadasGanadas){
+    int jugadasTotales = N * (N - 1) / 2;
+
+    return 1 - (double)jugadasGanadas / jugadasTotales;
 }
 
 int main() {
     cin >> N >> M;
-    backConExtremoInferiorEn.resize(N + 1, 0);
-    backConExtremoSuperiorEn.resize(N + 1, 0);
     g.resize(N + 1, vector<int>(0));
-    estado.resize(N + 1, NO_LO_VI);
-    memo.resize(N + 1, -1);
-    treeEdges.resize(N + 1, vector<int>(0));
 
     for (int i = 0; i < M; i++) {
         int u, v;
@@ -86,7 +114,8 @@ int main() {
         g[v].push_back(u);
     }
 
-    double res = calcularJugadas();
+    int ganadas = calcularJugadasGanadas();
+    double res = probaPerder(ganadas);
 
     cout << fixed;
     cout << setprecision(5) << res;
