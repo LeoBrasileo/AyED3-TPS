88c860bd33867f0dc9576b19e24411fd1f909ce7
LeoBrasileo <leobraginski@gmail.com>
2023-05-24 12:04:01 -0300

    emprolijado 3

--- TP2/Ej3/main.cpp
+++ TP2/Ej3/main.cpp
@@ -2,65 +2,62 @@
 
 using namespace std;
 
-typedef pair<int,int> arista;
-using ll = long long;
-const ll inf = 1e18;
+typedef pair<int, int> arista;
 
 int N, R, W, U, V;
 double costoUtp = 0, costoFibra = 0;
-vector<tuple<double,int,int,bool>> E, AGM;
-vector<vector<pair<int, double>>> g;
+vector<tuple<double, int, int, bool>> E, AGM;
 
-struct DSU{
-    DSU(int n){
+struct DSU {
+    DSU(int n) {
         padre = rank = vector<int>(n);
-        for(int v = 0; v < n; v++) padre[v] = v;
+        for (int v = 0; v < n; v++) padre[v] = v;
     }
-    int find(int v){
-        if(v == padre[v]) return v;
+
+    int find(int v) {
+        if (v == padre[v]) return v;
         return padre[v] = find(padre[v]);
     }
-    void unite(int u, int v){
+
+    void unite(int u, int v) {
         u = find(u), v = find(v);
-        if(u == v) return;
-        if(rank[u] < rank[v]) swap(u,v);
+        if (u == v) return;
+        if (rank[u] < rank[v]) swap(u, v);
         padre[v] = padre[u];
-        rank[u] = max(rank[u],rank[v]+1);
+        rank[u] = max(rank[u], rank[v] + 1);
     }
+
     vector<int> padre;
     vector<int> rank;
 };
 
-void kruskal(){
+void kruskal() {
     //basado en implementacion vista en clase
-    sort(E.begin(),E.end());
-    int aristas = 0;
+    sort(E.begin(), E.end());
     DSU dsu(N);
-    for(auto [c,u,v,utp] : E){
+    for (auto [c, u, v, utp]: E) {
         //si (u,v) es arista segura
-        if(dsu.find(u) != dsu.find(v)){
-            dsu.unite(u,v);
-            aristas++;
-            AGM.push_back({c,u,v,utp});
+        if (dsu.find(u) != dsu.find(v)) {
+            dsu.unite(u, v);
+            AGM.push_back({c, u, v, utp});
         }
     }
-    if(aristas == N-1) {
+    if (AGM.size() == N - 1) {
         //saco las W-1 aristas mas pesadas
-        for (int i = 0; i < W-1; i++){
+        for (int i = 0; i < W - 1; i++)
             AGM.pop_back();
-        }
 
-        for(auto [c,u,v,utp] : AGM){
+        for (auto [c, u, v, utp]: AGM) {
             if (utp) costoUtp += c;
             else costoFibra += c;
         }
     }
 }
 
-double distancia(int v1, int u1, int v2, int u2){
-    int distV = v1-v2;
-    int distU = u1-u2;
-    return sqrt(distV*distV + distU*distU);
+double distancia(int v1, int u1, int v2, int u2) {
+    int distV = v1 - v2;
+    int distU = u1 - u2;
+    return sqrt(distV * distV + distU * distU);
 }
 
 int main() {
@@ -68,23 +65,22 @@ int main() {
     cin >> C;
     for (int i = 1; i <= C; i++) {
         cin >> N >> R >> W >> U >> V;
-        g = vector<vector<pair<int, double>>>(N);
         vector<arista> aristas;
-        for(int j = 0; j < N; j++){
+        for (int j = 0; j < N; j++) {
             int x, y;
             cin >> x >> y;
-            aristas.push_back({x,y});
+            aristas.push_back({x, y});
         }
 
         //armado de aristas con pesos
-        for(int ii = 0; ii < N; ii++){
-            for(int j = ii+1; j < N; j++){
+        for (int ii = 0; ii < N; ii++) {
+            for (int j = ii + 1; j < N; j++) {
                 arista a = aristas[ii];
                 arista b = aristas[j];
-                double costo = distancia(a.first,a.second,b.first,b.second);
+                double costo = distancia(a.first, a.second, b.first, b.second);
                 bool utp = costo <= R;
-                costo = costo <= R ? costo * U : costo * V; //si es menor a R, uso UTP, sino fibra optica
-                E.push_back({costo,ii,j, utp});
+                costo = utp ? costo * U : costo * V;
+                E.push_back({costo, ii, j, utp});
             }
         }
 
@@ -96,7 +92,6 @@ int main() {
         costoFibra = costoUtp = 0;
         AGM.clear();
         E.clear();
-        g.clear();
     }
 
     return 0;
