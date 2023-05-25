f5fd47740c4ea9df739fc4b776922eeac2b7b607
gastondeganis <gastondeganis@gmail.com>
2023-05-25 19:37:23 -0300

    arregle xdddd

--- TP2/Ej3/experimentacion/inputs/generar_random.py
+++ TP2/Ej3/experimentacion/inputs/generar_random.py
@@ -13,6 +13,10 @@ V = input(f"Escribí el V, tiene que ser mayor igual a {U}\n")
 while(int(V)<int(U) or int(V)>10):
 	V = input(f"V tiene que ser mayor igual a {U} y menor igual que 10. Probá otra vez\n")
 
+R = input(f"Escribí el R\n")
+while(int(R)>10000):
+	R = input(f"R tiene que ser menor igual que 10000. Probá otra vez\n")
+
 file = open(f"{in_txt}.txt", "w")
 
 file.write("4950\n")
@@ -34,7 +38,7 @@ n = 0
 while n<1000:
 	w=0
 	while w<n:
-		line = f"{n} {w} {U} {V}\n"
+		line = f"{n} {R} {w} {U} {V}\n"
 		file.write(line)
 		generar_pares_unicos(n)
 		w+=10
