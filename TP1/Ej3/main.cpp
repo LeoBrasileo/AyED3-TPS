93c71078c7a6772107448039ab2e5fd5312bfaf6
LeoBrasileo <leobraginski@gmail.com>
2023-07-11 23:59:56 -0300

    TP1 code corrections

--- TP1/Ej3/main.cpp
+++ TP1/Ej3/main.cpp
@@ -1,96 +1,84 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-typedef pair<int, int> oficina;
-
-int N, R, W, U, V;
-double costoUtp = 0, costoFibra = 0;
-vector<tuple<double, int, int, bool>> E, AGM;
-
-struct DSU {
-    DSU(int n) {
-        padre = rank = vector<int>(n);
-        for (int v = 0; v < n; v++) padre[v] = v;
-    }
-
-    int find(int v) {
-        if (v == padre[v]) return v;
-        return padre[v] = find(padre[v]);
-    }
-
-    void unite(int u, int v) {
-        u = find(u), v = find(v);
-        if (u == v) return;
-        if (rank[u] < rank[v]) swap(u, v);
-        padre[v] = padre[u];
-        rank[u] = max(rank[u], rank[v] + 1);
-    }
-
-    vector<int> padre;
-    vector<int> rank;
+struct Actividad {
+    int inicio;
+    int final;
+    int posicion;
 };
 
-void kruskal() {
-    //basado en implementacion vista en clase
-    sort(E.begin(), E.end());
-    DSU dsu(N);
-    int componentes = N;
-    for (auto [c, u, v, utp]: E) {
-        if (componentes == W) break;
-        //si (u,v) es arista segura
-        if (dsu.find(u) != dsu.find(v)) {
-            dsu.unite(u, v);
-            AGM.push_back({c, u, v, utp});
-            componentes--;
-        }
+// Variable global
+int N = 0;
+vector<Actividad> actividades;
+
+void sortDeActividades(vector<Actividad> &actividades){
+    // como el rango de horas está acotado por 2N uso Counting Sort
+
+    vector<Actividad> ordenadas[(2*N)+1];
+
+    for (int i=0; i<N; i++){
+        ordenadas[actividades[i].final].push_back(actividades[i]);
     }
 
-    for (auto [c, u, v, utp]: AGM) {
-        if (utp) costoUtp += c;
-        else costoFibra += c;
-    }
-}
-
-double distancia(int v1, int u1, int v2, int u2) {
-    int distV = v1 - v2;
-    int distU = u1 - u2;
-    return sqrt(distV * distV + distU * distU);
-}
-
-int main() {
-    int C;
-    cin >> C;
-    for (int i = 1; i <= C; i++) {
-        cin >> N >> R >> W >> U >> V;
-        vector<oficina> oficinas;
-        for (int j = 0; j < N; j++) {
-            int x, y;
-            cin >> x >> y;
-            oficinas.push_back({x, y});
-        }
-
-        //armado de aristas con pesos
-        for (int ii = 0; ii < N; ii++) {
-            for (int j = ii + 1; j < N; j++) {
-                oficina a = oficinas[ii];
-                oficina b = oficinas[j];
-                double costo = distancia(a.first, a.second, b.first, b.second);
-                bool utp = costo <= R;
-                costo = utp ? costo * U : costo * V;
-                E.push_back({costo, ii, j, utp});
+    int i = 0;
+    for (int j = 0; j < (2*N) + 1; j++){
+        if (ordenadas[j].size() != 0){
+            for(int k = 0; k < ordenadas[j].size(); k++){
+                actividades[i] = ordenadas[j][k];
+                i++;
             }
         }
-
-        kruskal();
-
-        cout << "Caso #" << i << ": " << fixed << setprecision(3) << costoUtp << " " << costoFibra << "\n";
-
-        //limpiar para prox test
-        costoFibra = costoUtp = 0;
-        AGM.clear();
-        E.clear();
     }
+
+    return;
+}
+
+
+vector<Actividad> maximizarActividades(){
+
+    // Ordeno las actividades
+    sortDeActividades(actividades);
+
+    vector<Actividad> respuesta;
+    respuesta.push_back(actividades[0]);
+    Actividad* ultima = &respuesta.back();
+    for (int i = 1; i < N; i++){
+        if (actividades[i].inicio >= ultima->final){
+            respuesta.push_back(actividades[i]);
+            ultima = &respuesta.back();
+        }
+    }
+
+    return respuesta;
+}
+
+
+int main() {
+    cin >> N;
+
+    actividades.resize(N);
+    for (int i = 0; i < N; i++){
+        int s, t; cin >> s >> t;
+        actividades[i].inicio = s;
+        actividades[i].final = t;
+        actividades[i].posicion = i+1;
+    }
+
+    vector<Actividad> respuesta = maximizarActividades();
+
+    // Imprimir salida
+    int length = respuesta.size();
+    cout << length << endl;
+
+    for (int i = 0; i < length; i++){
+        cout << respuesta[i].posicion;
+        if(i != length-1){
+            cout << " ";
+        }
+    }
+    cout << endl;
 
     return 0;
 }
